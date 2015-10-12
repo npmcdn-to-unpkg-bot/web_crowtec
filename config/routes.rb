8aa66121f893fcc0b89467bbb46830276dd297b7
Meriem El Yamri <melyamri@gmail.com>
2015-10-12 20:27:32 +0200

    about section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'awards/index'
+
+  get 'work/index'
+
+  get 'services/index'
+
   root 'welcome#index'
+
+  get 'about' => 'about#index', :as => :about
+  get 'services' => 'services#index', :as => :services
+  get 'work' => 'work#index', :as => :work
+  get 'awards' => 'awards#index', :as => :awards
 
 end
